34ef7134d9b269af69899122cac91a284f228a72
Tirth Dudhat <tirth1644@gmail.com>
2024-06-07 18:13:12 +0530

    footer

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+hide_st_style = '''
 
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+'''
+
+st.markdown(hide_st_style, unsafe_allow_html=True)
 st.logo(image='logo.png')
 import hero
 import intro
