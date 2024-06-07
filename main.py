1e9b33a4b581023da0d811a0414a358e3a612c10
Tirth Dudhat <tirth1644@gmail.com>
2024-06-07 18:28:53 +0530

    hero

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ header {visibility: hidden;}
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.logo(image='logo.png')
+col1, col2 = st.columns(2)
+with col1:
+    st.title(':blue[Your Competitors Have  Fresh Data—  Do You?]')
+    st.markdown("We believe success and failure often hinges on having the most accurate information")
+    # st.markdown(" Imagine making a crucial business decision with outdated data—how would that impact your confidence and results?")
+    st.markdown("That's why we exist: with our web scraping expertise and free monthly data refreshes, you'll always be steps ahead.")
+    st.button('**Contact Now**',type='primary')
+    st.button('See Pricing')
+with col2:
+    st.image("banner.jpg")
+st.divider()
 import hero
 import intro
 import projects
